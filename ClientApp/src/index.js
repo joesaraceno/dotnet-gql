3fbf0761c195c6ffc02bba8fecb3be1212b8b6cb
joesaraceno <josephasaraceno@gmail.com>
2019-06-19 15:40:25 -0600

    import apollo hooks provider

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import {ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import ApolloClient from 'apollo-boost';
+
 import './index.css';
 import './App.css';
+
 import Products from './components/Products';
-
-import * as serviceWorker from './serviceWorker';
-
-import ApolloClient from "apollo-boost";
-
-import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -16,19 +15,11 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        <h1>Product List</h1>
-        <Products />
-      </header>
-    </div>
+    <ApolloHooksProvider client={client}>
+      <Products />
+    </ApolloHooksProvider>
   </ApolloProvider>
 )
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
