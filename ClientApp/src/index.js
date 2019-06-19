b401b0ef3a09596309662dd038faac81ea3cc376
joesaraceno <josephasaraceno@gmail.com>
2019-06-19 16:40:17 -0600

    fix up app.js

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import ApolloClient from 'apollo-boost';
-
-import './index.css';
-import './App.css';
-
-import Products from './components/Products';
-
-const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
-});
-
-const App = () => (
-  <ApolloProvider client={client}>
-    <ApolloHooksProvider client={client}>
-      <Products />
-    </ApolloHooksProvider>
-  </ApolloProvider>
-)
-
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
